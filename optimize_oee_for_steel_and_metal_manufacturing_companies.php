00e958096941e38afcfc0407c4ceeb15824e967b
mohit <mohitpurbia00@gmail.com>
2020-01-09 13:12:49 +0530

    added modal for case study

--- optimize_oee_for_steel_and_metal_manufacturing_companies.php
+++ optimize_oee_for_steel_and_metal_manufacturing_companies.php
@@ -516,7 +516,7 @@
                                 <h3 class="fw-900">a full case study</h3>
                             </div>
                             <div>
-                                <a href="blogs" class="btn btn-round btn-white">Click Here</a>
+                                <a href="#" data-toggle="modal" data-target="#exampleModal" class="btn btn-round btn-white">Click Here</a>
                             </div>
                         </div>
                     </div>
@@ -537,11 +537,34 @@
             </div>
         </div>
 
+
     <!------------------------footer start --------------------->
     <?php
                     include 'footer.php';
                     ?>
     <!------------------------footer end --------------------->
+
+    <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Modal -->
 
     <div class="cookieFooter bg-red-grd" id="cookieDiv">
         <div class="container">
