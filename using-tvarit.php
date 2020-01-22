3fc47be69fa9ce07aa331afd2ba6be65bebcba7e
eliteinfoway <59756790+eliteinfoway@users.noreply.github.com>
2020-01-22 11:18:15 +0530

    Ai platform (#9)
    changes for home page,  title and footer,  typo fix

--- using-tvarit.php
+++ using-tvarit.php
@@ -1,46 +1,44 @@
-<div class="section-padding-both">
-      <div class="container">
-        <div class="centerTitle text-big60">
-        References 
-        <!-- <span class="tvarit-black">Tv<span class="color">a</span>r<span class="color">i</span>t</span> -->
-        </div>
-        <br>
-        <div class="client-carousel">
-          <div class="carousel-cell">
-            <div class="picBox w-90">
-              <img src="img/client_logo/aditya_birla.jpg" class="img-fluid">
-            </div>
-          </div>
-          <div class="carousel-cell">
-            <div class="picBox w-90">
-              <img src="img/client_logo/intel.png" class="img-fluid">
-            </div>
-          </div>
-          <div class="carousel-cell">
-            <div class="picBox">
-              <img src="img/client_logo/NTT.png" class="img-fluid">
-            </div>
-          </div>
-          <div class="carousel-cell">
-            <div class="picBox">
-              <img src="img/client_logo/times-of-india.png" class="img-fluid">
-            </div>
-          </div>
-          <div class="carousel-cell">
-            <div class="picBox">
-              <img src="img/client_logo/e-opt.jpg" class="img-fluid">
-            </div>
-          </div>
-          <div class="carousel-cell">
-            <div class="picBox">
-              <img src="img/client_logo/Avrio.png" class="img-fluid">
-            </div>
-          </div>
-          <div class="carousel-cell">
-            <div class="picBox">
-              <img src="img/client_logo/Siemens.png" class="img-fluid">
-            </div>
-          </div>
-        </div>
+<div class="container">
+  <div class="centerTitle sec-mainTitle">
+  References 
+  <!-- <span class="tvarit-black">Tv<span class="color">a</span>r<span class="color">i</span>t</span> -->
+  </div>
+  <br>
+  <div class="client-carousel REFERENCES-carousel">
+    <div class="carousel-cell">
+      <div class="picBox w-90">
+        <img src="img/client_logo/aditya_birla.jpg" class="img-fluid">
       </div>
     </div>
+    <div class="carousel-cell">
+      <div class="picBox w-90">
+        <img src="img/client_logo/intel.png" class="img-fluid">
+      </div>
+    </div>
+    <div class="carousel-cell">
+      <div class="picBox">
+        <img src="img/client_logo/NTT.png" class="img-fluid">
+      </div>
+    </div>
+    <div class="carousel-cell">
+      <div class="picBox">
+        <img src="img/client_logo/times-of-india.png" class="img-fluid">
+      </div>
+    </div>
+    <div class="carousel-cell">
+      <div class="picBox">
+        <img src="img/client_logo/e-opt.jpg" class="img-fluid">
+      </div>
+    </div>
+    <div class="carousel-cell">
+      <div class="picBox">
+        <img src="img/client_logo/Avrio.png" class="img-fluid">
+      </div>
+    </div>
+    <div class="carousel-cell">
+      <div class="picBox">
+        <img src="img/client_logo/Siemens.png" class="img-fluid">
+      </div>
+    </div>
+  </div>
+</div>
