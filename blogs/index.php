0930d286412fe4b87ccd06f5edaf93b37a984651
Indra <indrakumarprajapat@gmail.com>
2020-01-23 10:33:48 +0530

    console error fixes and added img

--- blogs/index.php
+++ blogs/index.php
@@ -530,8 +530,29 @@
     <script src="../js/mixitup.min.js"></script>
     <script src="../js/script.js"></script>
     <script src="../js/parallax.js"></script>
+    <script>
+        var containerEl = document.querySelector('.filter-container');
+        var checkboxGroup = document.querySelector('.checkbox-filter');
+        var checkboxes = checkboxGroup.querySelectorAll('input[type="checkbox"]');
+        var mixer = mixitup(containerEl);
 
+        checkboxGroup.addEventListener('change', function() {
+            var selectors = [];
+            // Iterate through all checkboxes, pushing the
+            // values of those that are checked into an array
+            for (var i = 0; i < checkboxes.length; i++) {
+                var checkbox = checkboxes[i];
 
+                if (checkbox.checked) selectors.push(checkbox.value);
+            }
+            // If there are values in the array, join it into a string
+            // using your desired logic, and send to the mixer's .filter()
+            // method, otherwise filter by 'all'
+            var selectorString = selectors.length > 0 ?
+                selectors.join(',') : // or '.' for AND logic
+                'all';
+            mixer.filter(selectorString);
+        });
+    </script>
 </body>
-
 </html>
