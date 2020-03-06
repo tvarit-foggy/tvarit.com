321cf7bfd582889ae12860b629dfdd2c448c3ee4
mohit <mohitpurbia00@gmail.com>
2020-03-06 15:58:54 +0530

    hide loading on page load

--- contact.php
+++ contact.php
@@ -280,6 +280,10 @@ $("#btn").live("click",function(){
     <script src="js/script.js"></script>
     <script src="js/parallax.js"></script>
     <script>
+    	$(window).load(function() {
+		// Animate loader off screen
+		$(".fadeMe").fadeOut("slow");;
+	});
      function validate(evt) {
         var theEvent = evt || window.event;
 
