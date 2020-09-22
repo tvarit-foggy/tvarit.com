7a005a8c1ba4a0f2482e9086a34aec9707268441
Indra KP <indrakumarprajapat@gmail.com>
2020-09-22 17:52:25 +0530

    office 365 email test

---
+++ emailtest.php
@@ -0,0 +1,34 @@
+<?php
+  use PHPMailer\PHPMailer\PHPMailer;
+  require 'vendor/autoload.php';
+  
+  $mail = new PHPMailer();
+  $mail->IsSMTP();
+
+  $mail->SMTPDebug  = 0;  
+  $mail->SMTPAuth   = TRUE;
+  $mail->SMTPSecure = "tls";
+  $mail->Port       = 587;
+  $mail->Host       = "smtp.office365.com";
+  $mail->Username   = "web@tvarit.com";
+  $mail->Password   = "Th3syst3mhasfailed123@";
+
+  $mail->IsHTML(true);
+  $mail->AddAddress("web@tvarit.com", "indra kp");
+  $mail->SetFrom("web@tvarit.com", "INDRA A");
+  $mail->AddReplyTo("indrakumarprajapat@gmail.com", "indrakkk");
+//   $mail->AddCC("cc-recipient-email", "cc-recipient-name");
+  $mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
+  $content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
+
+  $mail->MsgHTML($content); 
+  if(!$mail->Send()) {
+    echo "Error while sending Email.";
+    echo "<br>";
+    echo "<br>";
+    var_dump($mail);
+  } else {
+    echo "<br>";
+    echo "Email sent successfully";
+  }
+?>
