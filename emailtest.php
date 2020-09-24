5fa373d21e8d3b66310c892da374aa8f902e794a
Indra KP <indrakumarprajapat@gmail.com>
2020-09-24 16:31:22 +0530

    Testing Email

--- emailtest.php
+++ emailtest.php
@@ -10,14 +10,14 @@
   $mail->SMTPAuth   = TRUE;
   $mail->SMTPSecure = "tls";
   $mail->Port       = 587;
-  $mail->Host       = "smtp.office365.com";
-  $mail->Username   = $mail_from;
-  $mail->Password   = $mail_password;
+  $mail->Host       = "smtp.gmail.com";
+  $mail->Username   = "gkuda24@gmail.com";
+  $mail->Password   = "insessor13579";
 
   $mail->IsHTML(true);
-  $mail->AddAddress($mail_from, "indra kp");
-  $mail->SetFrom($mail_from, "INDRA A");
-  $mail->AddReplyTo("indrakumarprajapat@gmail.com", "indrakkk");
+  $mail->AddAddress("indra@insessor.com", "Sumesh");
+  $mail->SetFrom($mail->Username, "INDRA A");
+  $mail->AddReplyTo($mail->Username, "indrakkk");
 //   $mail->AddCC("cc-recipient-email", "cc-recipient-name");
   $mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
   $content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
