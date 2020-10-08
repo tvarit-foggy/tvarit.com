7b0b702b50effe2d846c9f71dd86a6e0f5c9da6c
sumeshks29 <sumeshks29@gmail.com>
2020-10-08 16:26:38 +0530

    added subscription terms

---
+++ subscription-terms.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <script src="js/navigationDE.js"></script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-143837547-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-143837547-1');
+    </script>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="site" content="tvarit.com" />
+    <meta name="keywords"
+        content="SPS – Smart Production Solutions, Nuremberg, NürnbergMesse, Components, Control Technology, Drive Systems, Electromechanical Components, Human-Machine-Interface Devices, Industrial Communication, Industrial Software, Interface Technology, IPCs, Motion Control, Peripheral Equipment, Sensor Technology, " />
+    <meta name="description"
+        content="Tvarit AI is the best solution for smart manufacturing, smart machines and digital transformation" />
+    <meta name="keywords"
+        content="machine learning, free, no credit card, big data, machine learning, ml, machine learning as a service, machine learning API, API, datasets, models, decision trees, predictive models, predictions, fast predictions, evaluation, evaluate predictive models, ensembles, random decision forest, boosted trees, gradient boosted trees, gradient boosting, boosting ensembles, isolation forest, anomaly detector, anomaly score, clustering, k-means, g-means, cluster, centroids, linear regression, regression, logistic regression, lr, topic, topic modeling, topic distributions, PCA, principal component analysis, text analysis, LDA, Latent Dirichlet Allocation, partial dependence plot, scatter plot, time series forecasting, timeseries, forecast, trends, deepnets, deep learning, deep neural networks, neural network, neural networks, automl, automatic optimization, optiml, fusion, easy, model gallery, workflows, flatline, whizzml, dsl, domain-specific language, bindings, library, amazon echo, alexa, mac, desktop" />
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="./assets/img/kit/free/apple-icon.png">
+    <link rel="icon" href="./img/favicon.png">
+    <title>Subscription and Cancellation Terms</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/flickity.css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300i,400,400i,700,700i" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+        integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+    <link href="https://unpkg.com/ionicons@4.5.0/dist/css/ionicons.min.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+    <script type="text/javascript" src="https://secure.perk0mean.com/js/173652.js"></script>
+    <noscript><img alt="" src="https://secure.perk0mean.com/173652.png" style="display:none;" /></noscript>
+    <!------------------------ main menu start ---------------------->
+    <?php 
+     include 'header.php';
+     ?>
+    <!------------------------ main menu end ------------------------>
+
+
+
+
+ 
+    <div class="section-padding-both-2x" id="Philanthropy">
+        <div class="container">
+            <h4>Tvarit GmbH Subscription and Cancellation Terms</h4>
+            <h3>Subscrption Terms</h3>
+            <h2>Month to Month Billing</h2>
+            <p>
+                Your membership starts when your underlying invoice is generated. 
+                Your membership will consequently reestablish every month without notice until you cancel the subscription. 
+                You approve us to store your installment method(s) and to automatically charge your installment method(s) 
+                consistently until you cancel the subscription. We will consequently charge you the then-current rate 
+                for your arrangement, in addition to appropriate duties, (for example, VAT or GST if the rate does exclude it), 
+                consistently upon renewal until you cancel the subscription. 
+            </p>
+            <p>
+                We may change your subscription's rate every month to month renewal term and we will notify 
+                you of any rate change with the alternative to cancel the subscription. In the event that the 
+                pertinent VAT or GST rate (or other included expense or obligation) changes during your one-month 
+                term, we will appropriately modify the duty comprehensive cost for your subscription on your next billing date.  
+            </p>
+            <p>
+                If your primary payment method fails, you authorize us to charge any other payment method in your account. 
+                If you have not provided us a backup payment method(s) and you fail to provide payment or if all payment 
+                methods in your account fail, we may suspend your subscription. You can edit your payment information any 
+                time in your Adobe Account page.   
+            </p>
+            <p>
+                For European Economic Area customers, your bank may require you to authenticate your initial purchase 
+                using a password, a one-time code sent to your mobile number or biometric recognition. When you authenticate, 
+                you also authorize us to charge your payment method for your additional purchases without providing us further 
+                payment information or other instructions (i.e., we will initiate future payments independently). Such additional 
+                purchases may occur when we automatically charge your payment method in connection with a recurring subscription or 
+                when you add or change licenses or products.    
+            </p>
+            <h3>Cancellation Terms</h3>
+                You can cancel your subscription any time by contacting Customer Support. If you cancel your service will 
+                continue until the end of that month’s billing period. 
+
+        </div>
+    </div>
+
+
+    <?php
+    include 'footer.php'
+    ?>
+
+    <!------------------------try demo start --------------------->
+    <style>
+    table {
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    td,
+    th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+    }
+
+    tr:nth-child(even) {
+        background-color: #dddddd;
+    }
+
+    .mainHeading {
+        font-weight: 700;
+        font-size: 20px;
+    }
+
+    .subHeading {
+        font-weight: 700;
+        font-size: 15px;
+    }
+    </style>
+    <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-1.7.2.min.js"></script>
+
+    <!------------------------try demo end --------------------->
+    <script>
+    $(window).load(function() {
+        // Animate loader off screen
+        $(".fadeMe").fadeOut("slow");;
+    });
+    </script>
+
+    <!-- script start -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+
+    <script src="js/flickity.pkgd.min.js"></script>
+    <script src="js/readmore.js"></script>
+    <script src="js/counting.js"></script>
+    <script src="js/script.js"></script>
+    <script src="js/parallax.js"></script>
+    <script src="js/SmoothScroll.js"></script>
+
+
+</body>
+
+</html>
