dc2a9d36eb4ee75419b07aa08a4bebe777a56ae3
digitalwebair <72067881+digitalwebair@users.noreply.github.com>
2020-12-08 11:57:06 +0530

    update reference video

--- using-tvarit.php
+++ using-tvarit.php
@@ -9,7 +9,7 @@
             <div class="row align-items-center">
                 <div class="col-lg-6">
                     <div class="videoWrapper">
-                            <iframe class="videoFrame" src="https://www.youtube.com/embed/_fMfc3qS1-s" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen  rel="0"></iframe>
+                            <iframe class="videoFrame" src="https://www.youtube.com/embed/_fMfc3qS1-s&rel=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                 </div>
                 <div class="col-lg-6">
@@ -19,7 +19,7 @@
                         </div>
                         <div class="short-line bg-red"></div>
                         <p class="text-p">
-                       <i>Tvarit has one of the best artificial intelligence technologies that I have ever seen".</i> Learn more on what Prof. Aitor Goti Elordi, Director of the Digital Industry chair of the University of Deusto, has to say about his experiences in working with Tvarit.</p>
+                       <i>"Tvarit has one of the best artificial intelligence technologies that I have ever seen".</i> Learn more on what Prof. Aitor Goti Elordi, Director of the Digital Industry chair of the University of Deusto, has to say about his experiences in working with Tvarit.</p>
                         <br>
                     </div>
 
