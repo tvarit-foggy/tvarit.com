e18dffe51929f960a50b4ef68e0715a1fe253604
Lokesh Mittal <lokeshmittaldev@gmail.com>
2021-01-30 19:45:29 +0530

    Updated auto email formatting-Contact forms

--- mail.php
+++ mail.php
@@ -8,20 +8,19 @@
   $msg = $_POST["msg"];
   $to = "$email";
   $message = "
-  Dear <b> $name </b>, <br><br>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b> Thank you very much for contacting us. here is your message. Our team will contact you soon.</b>
-             <br><br>
-  Email:  &nbsp;&nbsp; $email <br>  
-  Phone:  &nbsp;&nbsp; $phone <br>
-  Subject: &nbsp;&nbsp;$subject <br>
-  Message: &nbsp;&nbsp;$msg  <br><br>
-  
-  Kind Regards,<br>
-  Tvarit Team<br>
-  <br><br>
-  Tvarit GmbH<br>
+<div style=\"font-size:22px; line-height:30px; margin-bottom:30px\"><img src='https://www.tvarit.com/img/contactBg.jpg'/><div>
+<div style=\"font-size:22px; line-height:30px; text-transform:uppercase;\">Dear<b> $name </b>,<div>
+<div style=\"margin:30px 0px 20px 0px;\">Thank you very much for reaching out to us. We will be happy to assist you further by responding to your inquiry as soon as possible.</div>
+
+<div><strongHere’s is your message</strong> (<strong>$msg</strong>)</div>
+
+<div style=\"margin:20px 0px 20px 0px;\"><strong>Best Regards,</strong><br> Tvarit Team<br><strong>Mail:</strong> <a href=\"info@tvarit.com\">info@tvarit.com</a><br><strong>Web:</strong> <a href='https://www.tvarit.com'>www.tvarit.com</a><br><strong>Follow us on:</strong> <a href='https://www.linkedin.com/company/13015039'>Linkidin</a><br>EU H2020 winner of the best Industrial AI solution among 490 AI companies in Europe.
+</div>
+
+  <strong>Tvarit GmbH</strong><br>
   Gundelandstrasse 5<br>
   60435 Frankfurt am Main<br>
+  Registergericht: Frankfurt am Main | Handelsregisternummer: HR B 114845<br>
   This message contains information that may be privileged or confidential and is the property of the Tvarit GmbH. It is intended only for the person to whom it is addressed. If you are not the intended recipient, you are not authorized to read, print, retain copy, disseminate, distribute, or use this message or any part thereof. If you receive this message in error, please notify the sender immediately and delete all copies of this message.
 ";
       $mail->IsHTML(true);
@@ -32,11 +31,11 @@
       $mail->addBCC($mail_from);
       $mail->Subject = $subject;
 
-      $mail->MsgHTML($message); 
+      $mail->MsgHTML($message);
       if($mail->send()){
         echo 'Message has been sent';
     }else{
         echo 'Message could not be sent.';
         echo 'Mailer Error: ' . $mail->ErrorInfo;
     }
-?>  
+?>
