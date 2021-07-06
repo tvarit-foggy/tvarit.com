129cc422ccdfa5a14cf8be427a8304974ef03c00
starkblaze01 <mayank.pathela@tvarit.com>
2021-07-06 17:19:10 +0530

    Removes first para

--- career.php
+++ career.php
@@ -547,9 +547,6 @@
                     <div id="SDSPS" class="collapse" aria-labelledby="SDSPS" data-parent="#accordion">
                         <div class="card-body">
                             <p class="text-p">
-                                Tvarit GmbH strives to help companies in the manufacturing industry transform into Industry 4.0 with the use of its unique AI software. The AI software we have developed is the most modern complete solution to solve many of our customers' business problems. You are responsible for building and improving the company's market position and for financial growth. You will be involved in defining long-term strategic business goals, building key customer relationships, identifying business opportunities, negotiating and closing deals, and having a thorough understanding of current market conditions. You should have a strong growth mindset and work ethic.
-                            </p>
-                            <p class="text-p">
                                 As a continuously expanding and award-winning technology company in a fast-growing market, we are looking for a ‘Senior Data Scientist – Pre-Sales’ (m/f/d) to support our Technical Pre-Sales Team in Frankfurt. Apply now and become part of our motivated, highly qualified and agile team
                             </p>
                             <h4>Your tasks:</h4>
